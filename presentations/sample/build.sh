5ec2534125d8c4a77db40484d46c8a2191655216
Werner Fouché <werner.fouche@gmail.com>
2025-07-30 13:18:37 +0200

    Updated presentation build files

---
+++ presentations/sample/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+jbang run asciidoctorj@asciidoctor \
+    -r asciidoctor-revealjs \
+    -r asciidoctor-diagram \
+    sample.adoc \
+    -b revealjs \
+    -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@5.2.0
